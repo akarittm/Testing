d2df952fc148a5555f5a713a6e30d2a4f6a019f6
Than Than Myint <ttmyint@isgm2.com>
2022-01-27 08:39:46 +0630

    add: laravel routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/articles', function () {
+    return 'Article List';
+});
+
+Route::get('/articles/detail', function () {
+    return 'Article Detail';
+})->name('article.detail');
+
+Route::get('/articles/detail/{id}', function ($id) {
+    return "Article Detail - $id";
 });
